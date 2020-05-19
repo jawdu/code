d3b80c7f4d6752dc593c904b2ca38a900589e39e
jason <64638179+jawdu@users.noreply.github.com>
2020-05-19 09:53:27 +0100

    python finish fractran

--- python/fractran.py
+++ python/fractran.py
@@ -1,21 +1,23 @@
 #! /usr/bin/python3
 
 # implemention of Conway's FRACTRAN programming language https://en.wikipedia.org/wiki/FRACTRAN 
-# also https://esolangs.org/wiki/Fractran
+# tested against Conway 1987 formula for primes
 
 # A FRACTRAN program is an ordered list of positive fractions together with an initial positive integer input n. The program is run by updating the integer n as follows:
 
 #   for the first fraction a/b in the list for which na/b is an integer, replace n by na/b
 #   repeat this rule until no fraction in the list produces an integer when multiplied by n, then halt.
 
-
 import re
 import sys
-# for exit
 
 a = []
 b = []
 k = 0
+
+p = 0
+q = 100000
+# to give early exits, adjust q as needed
 
 print("Enter fractions in sequence, in the form 'a/b', or any other expression to finish: ")
 
@@ -39,22 +41,20 @@ while True:
         n = int(n)
         break
 
-p = 0
-
 while k < len(a):
     if (n*a[k]/b[k]) % 1 == 0:
         n = n * a[k]/b[k]
         k = 0
         print("New n: ", n)
         p += 1
-        if p>1000:
+        if p>q:
             print("early exit new n loop")
             sys.exit()
     else:
         k += 1
-        print("Same n: ", n)
+        # print("Same n: ", n)
         p += 1
-        if p>1000:
+        if p>q:
             print("early exit same n loop")
             sys.exit()
 
