7e7f422b19cbd90e303fa53feed02b2c716fb63d
jason <64638179+jawdu@users.noreply.github.com>
2020-06-02 20:23:52 +0100

    finish sim v1

--- python/codeGolf/simUniv1.py
+++ python/codeGolf/simUniv1.py
@@ -11,22 +11,25 @@ u = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 l = ["Higgs boson", "W boson", "Z boson", "gluon", "charm quark", "anticharm quark", "strange quark", "strange antiquark", "top quark", "top antiquark", "bottom quark", "bottom antiquark", "up quark", "up antiquark", "down quark", "down antiquark", "muon", "antimuon", "tau lepton", "antitau lepton", "electron", "positron", "neutrino", "antineutrino", "photon"]  
 t = 0
 
+u[0] = int(input("Enter initial number of Higgs bosons: "))
+
 while (u[0] + u[1] + u[2] + u[12] + u[13]) != 0:
 
     t += 0.1
-    if (u[0] !=0) and (random.random() < 0.000433): #Higgs decay
-
-        u[0] -= 1; p = random.random()
-        if p<0.648: u[10] += 1; u[11] += 1
-        elif p<0.789: u[1] += 2
-        elif p<0.8772: u[3] += 2
-        elif p<0.9476: u[18] += 1; u[19] += 1
-        elif p<0.9803: u[4] += 1; u[5] += 1
-        elif p<0.9962: u[2] += 2
-        elif p<0.99843: u[24] += 2
-        elif p<0.99954: u[2] += 1; u[24] += 1
-        elif p<0.999784: u[16] += 1; u[17] += 1
-        else: u[8] += 1; u[9] += 1
+    if (u[0] !=0): #Higgs decay
+        for i in range(u[0]):
+           if random.random() < 0.000433:
+               u[0] -= 1; p = random.random()
+               if p<0.648: u[10] += 1; u[11] += 1
+               elif p<0.789: u[1] += 2
+               elif p<0.8772: u[3] += 2
+               elif p<0.9476: u[18] += 1; u[19] += 1
+               elif p<0.9803: u[4] += 1; u[5] += 1
+               elif p<0.9962: u[2] += 2
+               elif p<0.99843: u[24] += 2
+               elif p<0.99954: u[2] += 1; u[24] += 1
+               elif p<0.999784: u[16] += 1; u[17] += 1
+               else: u[8] += 1; u[9] += 1
 
     if u[1] != 0: #W decay
         for i in range(u[1]):
@@ -66,16 +69,16 @@ while (u[0] + u[1] + u[2] + u[12] + u[13]) != 0:
                 elif p<0.6666: u[11] += 1
                 else: u[15] += 1
 
-    print(u)   
-
-# ONLY need to format for last /first item in list
-
     update = ""
-    for i in range(24, 0, -1):
+    for i in range(24, -1, -1):
         if u[i] == 1:
-            update = str(u[i]) + " " + l[i] + ", " + update
+            if sum(u) == u[i]: update = str(u[i]) + " " + l[i] + "."
+            elif update == "": update = "and " + str(u[i]) + " " + l[i] + "."
+            else: update = str(u[i]) + " " + l[i] + ", " + update
         elif u[i] > 1:
-            update = str(u[i]) + " " + l[i] + "s," + update
+            if sum(u) == u[i]: update = str(u[i]) + " " + l[i] + "s."
+            elif update == "": update = "and " + str(u[i]) + " " + l[i] + "s."
+            else: update = str(u[i]) + " " + l[i] + "s, " + update
     print("The universe contains " + update)
 
 print("Simulation ended after %.1f yoctoseconds." % (t))
