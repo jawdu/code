aef7b6e1b010c9121a2f553b88a0e31a2ebabad7
jason <64638179+jawdu@users.noreply.github.com>
2020-06-10 22:07:19 +0100

    rF stuff

--- python/codeGolf/rForest.py
+++ python/codeGolf/rForest.py
@@ -43,6 +43,7 @@ for i in range(800):
 
 pStart = int(370 + np.random.uniform(-50, 50))
 pEnd = int(430 + np.random.uniform(-50, 50))
+pCentre = []
 h = 600 - horizon[pEnd]
 
 # add one sin from start, one from end, roughly randomised so they overlap in middle
@@ -50,7 +51,7 @@ h = 600 - horizon[pEnd]
 s1p = np.random.uniform(0.8, 1.2) * h/2 
 s2p = np.random.uniform(0.8, 1.2) * h/2 
 s1a = np.random.uniform(-25, 25) # more as closer
-2a = np.random.uniform(-15, 15) # less as further
+s2a = np.random.uniform(-15, 15) # less as further
 
 for i in range(599, horizon[pEnd], -1):
         pa = (pStart - pEnd) / h
@@ -64,12 +65,12 @@ for i in range(599, horizon[pEnd], -1):
                      
         # rough parameter of path width. start: 20 end: 5  -> gradient
         w = int((i * 20 / h) - 8)
+        pn = int(i * pa + pb + pc)
+        pCentre.append(pn)
 
         for j in range(-w, w):
             if (np.random.uniform(0, 1) < 0.15):
-                img.putpixel((int(i * pa + pb + j + pc), i) , (0, 0, 0))    
-
-
+                img.putpixel((pn+j, i) , (0, 0, 0))    
 
 boundary = np.random.normal(150, 5, size=(1, 800))
 
@@ -79,7 +80,7 @@ b2 = 23 + np.random.uniform(-3, 3)
 b3 = 360 + np.random.uniform(-20, 20)
 
 for i in range(800):
-    # melt boundary a little 
+    # melt boundary a little. maybe also add a bit of a abs|sin| to make it a little bumpy
     if (i < pEnd):
         boundary[0, i] = boundary[0, i] + np.random.uniform(b1, b2)*math.exp((pEnd-i)/b3)
     else:
@@ -94,13 +95,62 @@ for i in range(800):
         # fill in canopy; if to control density of canopy
         if (np.random.uniform(0, 1) < 0.25): img.putpixel((i, j), (0, 0, 0))
 
+tx = []; tw = []; ty = []; td = []
+
+k = np.random.randint(35, 60)
+
+# starting to feel need to control distribution more: e.g. by fixed interval over y *or* x
+
+for i in range(k):
+    if np.random.random() < 0.5: 
+        td.append(-1); tx.append(np.random.randint(500, 770))
+    else: 
+        td.append(1); tx.append(np.random.randint(30, 300))
+    ty.append(np.random.randint(300, 590))
+    tw.append(int(0.038 * ty[i] + np.random.uniform(10, 16)))
+
+
+# change width w.r.t. base: start more, then reduce for a few iterations
+# for i in tw/2: and increase density. tinker.
+
+# maybe do similar with top? fade out into canopy. might deal with bug.
+
+
+# 'tree half'
+#th = ty[0] - int((ty[0] - boundary[0,tx[0]])/2)
+#for i in range(ty[0], th, -1):
+
+for i in range(len(tx)): 
+
+    # do the base
+    for l in range(int(tw[i]/2)):
+        for k in range(int(-tw[i]/2), int(tw[i]/2)):
+            t =  0.2 * 2 * l / tw[i]
+            if (np.random.uniform(0,1) < t): img.putpixel((tx[i]+k, ty[i]), (0, 0, 0))
+        tw[i] -= 1
+        ty[i] -= 1
+
+# ^ base bit timid. maybe separate width paramater out, do 3 x width in x to give better spread
+
+    while ty[i] != 0:
+        for j in range(int(-tw[i]/2), int(tw[i]/2)):
+            if (np.random.uniform(0, 1) < 0.35): img.putpixel((tx[i]+j, ty[i]), (0, 0, 0))
+        ty[i] -= 1
+        if ((tx[i] - tw[i]/2) < 1) or ((tx[i] + tw[i]/2) > 799): 
+            ty[i] = 0
+        if ((ty[i] == boundary[0, tx[i]]) or (ty[i] == 140)): # 140 lazy bug fix. not working
+            ty[i] = 0
+        if (np.random.uniform(0, 1) < 0.2): tx[i] += int(np.random.uniform(-1,  2)) * td[i]
 
 
 
-# to do a tree. start with centre, thickness, #branch. random to add branch, then add centre, thickness
+# now start top half
 
-
-
+# maybe have seperate for trunk & branches
+# draw trees, and randomly save (x,y) point to draw branches afterwards
+# also better to build code for doing multiple trees 1st
+# also start from base & forget about doing top & bottom half
+        
 
 img.save(fileout, "PNG")
 
