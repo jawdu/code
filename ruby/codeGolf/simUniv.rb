9065c71506d1a7aa41a322e520fdb3b5e1e82114
jason <64638179+jawdu@users.noreply.github.com>
2020-07-01 16:58:15 +0100

    ongoing

--- ruby/codeGolf/simUniv.rb
+++ ruby/codeGolf/simUniv.rb
@@ -3,26 +3,44 @@
 #https://github.com/rubocop-hq/ruby-style-guide
 
 class Universe
-  def initialize(num_higgs)
-    @unstable = Array.[](num_higgs, 0, 0, 0, 0)
+  def initialize(num_unstable)
+    @unstable = Array.[](num_unstable, 0, 0, 0, 0)
+    # set up zero array
     @stable = Array.new(20, 0)
+    @time = 0
   end
 
   def show_data
-    # test
+    # to monitor stuff
     puts "unstable: #{@unstable}"
     puts "stable: #{@stable}"
+    puts "time: #{@time}"
+  end
+
+  def time_step
+    # do a timestep
+    # go backwards, avoid higgs->W/Z->other stuff in 1 timstep. still small chance (anti)top->W->other, meh
+
+
+    # housekeeping to finish
+    @time += 0.1
+    num_unstable = @unstable.inject(0, :+)
+    return num_unstable, @time
   end
 end
 
 print "Number of initial higgs in simulation: "
 
-num_higgs = gets.to_i
+num_unstable = gets.to_i
 
-u = Universe.new(num_higgs)
+u = Universe.new(num_unstable)
 u.show_data
 
-# while unstable
-#   u.timestep
-# end
+while (num_unstable > 0)
+  num_unstable, time = u.time_step
+end
+
+u.show_data
+
+printf("Simulation ended after %.1f yoctoseconds.", time)
 
