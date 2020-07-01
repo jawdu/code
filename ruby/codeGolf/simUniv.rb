023b0b563d833fdcae0b063cca356f74f8377c31
jason <64638179+jawdu@users.noreply.github.com>
2020-07-01 14:25:06 +0100

    keeping busy

--- ruby/codeGolf/simUniv.rb
+++ ruby/codeGolf/simUniv.rb
@@ -3,14 +3,24 @@
 #https://github.com/rubocop-hq/ruby-style-guide
 
 class Universe
+  def initialize(num_higgs)
+    @unstable = Array.[](num_higgs, 0, 0, 0, 0)
+    @stable = Array.new(20, 0)
+  end
 
+  def show_data
+    # test
+    puts "unstable: #{@unstable}"
+    puts "stable: #{@stable}"
+  end
 end
 
 print "Number of initial higgs in simulation: "
 
 num_higgs = gets.to_i
 
-u = Universe.new
+u = Universe.new(num_higgs)
+u.show_data
 
 # while unstable
 #   u.timestep
