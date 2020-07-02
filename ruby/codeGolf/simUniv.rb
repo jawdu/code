25df1d19e0544c37bcf2fca584d6bc6d873e052a
jason <64638179+jawdu@users.noreply.github.com>
2020-07-02 10:12:34 +0100

    ruby universe done w/out output

--- ruby/codeGolf/simUniv.rb
+++ ruby/codeGolf/simUniv.rb
@@ -23,31 +23,31 @@ class Universe
 
     if @unstable[4] > 0
       for i in 0..@unstable[4]
-        u.top_antitop(1) if rand < 0.1295
+        top_antitop(1) if rand < 0.1295
       end
     end
 
     if @unstable[3] > 0
       for i in 0..@unstable[3]
-        u.top_antitop(0) if rand < 0.1295
+        top_antitop(0) if rand < 0.1295
       end
     end
 
     if @unstable[2] > 0
       for i in 0..@unstable[2]
-          u.z_boson if rand < 0.5
+        z_boson if rand < 0.5
       end
     end
 
     if @unstable[1] > 0
       for i in 0..@unstable[1]
-        u.w_boson if rand < 0.5
+        w_boson if rand < 0.5
       end
     end
 
     if @unstable[0] > 0
       for i in 0..@unstable[0]
-        u.higgs if rand < 0.000433
+        higgs if rand < 0.000433
       end
     end
 
@@ -59,16 +59,94 @@ class Universe
 
   def top_antitop(top)
     # top/anti top symmetric. so use single method. add +top, 1 = 1 for anti
-
+    p = rand
+    @stable[3+top] -= 1
+    @stable[1] += 1
+    if p<0.3333
+      @stable[3+top] += 1
+    elsif p<0.6666
+      @stable[7+top] += 1 
+    else
+      @stable[9+top] += 1
+    end
   end
 
   def z_boson
+    @unstable[2] -= 1
+    p = rand
+    if p<0.206
+      @stable[17] += 1
+      @stable[18] +=1 
+    elsif p<0.24
+      @stable[14] += 1
+      @stable[15] +=1
+    elsif p<0.274
+      @stable[12] += 1
+      @stable[13] += 1
+    elsif p<0.308
+      @stable[10] += 1
+      @stable[11] += 1
+    elsif p<0.46
+      @unstable[4] += 1
+      @stable[9] += 1
+    elsif p<0.612
+      @stable[3] += 1
+      @stable[4] += 1
+    elsif p<0.764
+      @stable[7] += 1
+      @stable[8] += 1
+    elsif p<0.882
+      @unstable[3] += 1
+      @unstable[4] += 1
+    else
+        @stable[1] +=1
+        @stable[2] += 1
+    end
   end
 
   def w_boson
+    @unstable[1] -= 1
+    @stable[16] += 1
+    p = rand
+    if p<0.3333
+      @stable[12] += 1
+    elsif p<0.6666
+      @stable[14] += 1
+    else
+      @stable[15] += 1
+    end
   end
 
   def higgs
+    @unstable[0] -= 1
+    p = rand
+    if p<0.648
+      @stable[7] += 1
+      @stable[8] += 1
+    elsif p<0.789
+      @unstable[1] += 2
+    elsif p<0.8772
+      @stable[0] += 2
+    elsif p<0.9476
+      @stable[13] += 1
+      @stable[14] += 1
+    elsif p<0.9803
+      @stable[1] += 1
+      @stable[2] += 1
+    elsif p<0.9962
+      @unstable[2] += 2
+    elsif p<0.99843
+      @stable[19] += 2
+    elsif p<0.99954
+      @unstable[2] += 1
+      @stable[19] += 1
+    elsif p<0.999784
+      @stable[11] += 1
+      @stable[12] += 1
+    else
+      @stable[5] += 1
+      @stable[6] += 1
+    end
   end
 
 end
