9a218b8ab45c5b9818ab6931a2e1a0c89076f9a6
jason <64638179+jawdu@users.noreply.github.com>
2020-07-13 14:11:14 +0100

    start glitchino

---
+++ ruby/glitchino/glitchino.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+=begin
+    make a little glitch music
+=end
+
+require_relative 'gclass' 
+
+print "\n .'._ _'-_'..`` GLITCHINO '/ ,[- -!'' `|\n"
+
+
+print "\n ..,,done.,..,,\n\n"
+
