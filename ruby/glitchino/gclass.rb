ef5deed7b99f95bda33578e92ce185a29035147b
jason <64638179+jawdu@users.noreply.github.com>
2020-07-13 17:09:21 +0100

    modest advance

--- ruby/glitchino/gclass.rb
+++ ruby/glitchino/gclass.rb
@@ -1,7 +1,30 @@
 #!/usr/bin/env ruby
 
 class Glitch
+  def initialize(duration)
+    @waveform = Array.new
+  end
 
+  def test_noise
+    # setup basic glitchiness
+
+  end
+
+  def write_wav
+    # write to wav
+    fname = "glitchino." + Time.now.strftime("%H%M%S") + ".wav"
+
+    Writer.new(fname, Format.new(:mono, :pcm_16, 44100)) do |writer|
+        samples = ([0.5] * 100) + ([-0.5] * 100)
+  
+        buffer_format = Format.new(:mono, :float, 44100)
+        220.times do
+            buffer = Buffer.new(samples, buffer_format)
+            writer.write(buffer)
+        end
+    end
+
+  end
 end
 
 
--- ruby/glitchino/glitchino.rb
+++ ruby/glitchino/glitchino.rb
@@ -2,12 +2,28 @@
 
 =begin
     make a little glitch music
+    requires https://github.com/jstrait/wavefile
+        --> https://github.com/jstrait/wavefile/blob/master/examples/generate_square_wave.rb
+
+    step 1: generate some noise, make wav
 =end
 
-require_relative 'gclass' 
+require_relative 'gclass'
+require 'wavefile'
+include WaveFile
 
 print "\n .'._ _'-_'..`` GLITCHINO '/ ,[- -!'' `|\n"
 
+# get some parameters from user
+
+# new sound object
+
+g = Glitch.new(5) # 5 = duration in seconds
+
+# make wav.
+
+g.test_noise
+g.write_wav
 
 print "\n ..,,done.,..,,\n\n"
 
