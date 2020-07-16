694195c70f1cae2176aa8b0b8dc1019cf290471f
jason <64638179+jawdu@users.noreply.github.com>
2020-07-16 11:33:54 +0100

    jag

--- ruby/glitchino/gclass.rb
+++ ruby/glitchino/gclass.rb
@@ -15,12 +15,24 @@ class Glitch
   end
 
   def jag_noise(t0)
-    # make a sort of 'mountain' shape.
+    # make a sort of 'mountain' shape. linear between 5 points.
     # parameters to generate: t1...3, a1...3. a2 < a1, a3. zeros t0, t4
-    # think: what time units? seconds or sample rate?
-    t1 = t0 + rand
-    t2 = t1 + rand #etc
+    # think: what time units? seconds or sample rate? so multiply t0 * 41000 at 1st, or...
+    # probably keep as sec: got to convert to integer so save that until post-numerics
+    t0 = tsr(t0)
+    t1 = tsr(t0 + rand(0.001..0.002))
+    t2 = tsr(t1 + rand(0.001..0.002))
+    t3 = tsr(t2 + rand(0.001..0.002))
+    t4 = tsr(t3 + rand(0.001..0.002))
+    a1 = rand(0.4..0.8)
+    a3 = rand(0.4..0.8)
+    a2 = rand(0.1..[a1,a3].min)
 
+  end
+
+  def tsr(t)
+    # convert seconds to sample rate index integer
+    t = (44100*t).to_i
   end
 
   def add_synth
