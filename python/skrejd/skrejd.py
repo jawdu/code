ae70b761f3f500c065d6daa9fc49654594373944
jason <64638179+jawdu@users.noreply.github.com>
2020-07-30 11:13:08 +0100

    framework there

--- python/skrejd/skrejd.py
+++ python/skrejd/skrejd.py
@@ -5,6 +5,7 @@
 import sys
 import time
 import subprocess
+import numpy as np
 from scipy.io import wavfile
 
 filein = sys.argv[1]
@@ -14,26 +15,36 @@ if filein.endswith('.mp3'):
     subprocess.call(['ffmpeg', '-i', filein, fileout])
     filein = fileout
 elif not filein.endswith('.wav'):
-    # no file given
-    print("nope")
+    # no compatible file given
+    print("not a wav or mp3, exiting...")
     sys.exit()
 
-# read wav. sr = sample rate (/sec), audio numpy array with data
-sr, audio = wavfile.read(filein)
+# read wav. sr = sample rate (/sec), nt ndim (i.e. mono/stereo), audio np array audio[a,b] a=sound, |b| = 1,2 
+sr, audioIn = wavfile.read(filein)
+ns = audioIn.ndim
 
+# have to make copy, audio is just pointing at source. check if stereo
+if audioIn.ndim == 1: # make stereo
+    print("yes")
+    audio = np.array([audioIn, audioIn])
+    audio = np.transpose(audio)
+else: audio = audioIn.copy()
+
+ns = len(audio)
+for i in range(ns):
+    audio[i, 0] = 0.
+# ^ test - start adding stuff now
+
+
+# done, write to newfile and finish
 newfile = filein.rsplit('.', 1)[0] + '.' + time.strftime("%d%H%M%S") + '.wav'
-print(newfile)
-
-# change audio
-
+print("Written to ", newfile)
 wavfile.write(newfile, sr, audio)
-
 
 # ideas/todo:
 
-# command line list options. maybe also string of numbers to do multiple processes. random option(s)
 # input numeric string? so 642631 = do 6, then 4, then etc etc [but #fx? ]
 # if e.g. reverse, start from a wave[n] and find such that wave[n_2] = wave[n] and reverse between
 # e.g: modulate/flat noise / reverse bits / chop up / waveify / some quite drastic randomise stuff / use of chaotic systems
-
+# slow/speed up: add duplicate values (or just for l or r, then take away later on to conserve length)
 
