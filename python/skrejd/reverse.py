2a85f1c7590be4cb3f8a4dfdb0cc837dacd9e879
jason <64638179+jawdu@users.noreply.github.com>
2020-08-05 14:05:30 +0100

    mild revert

--- python/skrejd/reverse.py
+++ python/skrejd/reverse.py
@@ -4,11 +4,8 @@
 import random
 
 def reverse(ns, audio):
-    # get user inputs for these
-    lscale = int(ns / 120)     # length scale i.e. of each reverse segment
-    dscale = int(ns / 80)      # density scale i.e. gap between reverses
-    cscale = 0.4                   # prob of both channels
-
+    # main reverse function
+    lscale, dscale, cscale = revinput(ns)
     p1 = random.randint(lscale, 4*lscale)
     while (p1 < (ns - 4*lscale)):         # bit clumsy
         ch = random.randint(0, 1)       
@@ -31,6 +28,7 @@ def reverse(ns, audio):
         p1 = p2 + random.randint(dscale, dscale*3)                       
 
 def revseg(audio, p1, p2, s):
+    # reverse a segment in 1 channel
     tp = []
     for i in range(p1, p2):
         tp.append(audio[i,s])
@@ -39,4 +37,20 @@ def revseg(audio, p1, p2, s):
     for i in range(p1+1, p2):
         audio[i,s] = tp[p2-i]
    
+def revinput(ns):
+    # get user inputs, or defaults
+    # max in relation to ns: upper limit for lscale. dscale irrelevant (means just 1 reverse)
+    lscale = int(ns/120)        #input("Enter length scale in seconds, or '0' for default: ")
+    dscale = int(ns/80)         #input("Enter density scale in seconds, or '0' for default: ")
+    cscale = 0.4                #float(input("Enter decimal probability of both channel reverse, or '0' for default: "))
+
+# ___>> issues here. return to defaults, sort out input in seperate test programs
+# https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
+# want for lscale = f_name("message", type, min, max, default)
+# also put this elsewhere for use in other functions
+
+    return lscale, dscale, cscale
+
+
+
 
