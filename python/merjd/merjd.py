f85b0c1b23eddf5d2bf5ee3bc6720c4562293674
jason <64638179+jawdu@users.noreply.github.com>
2020-08-06 10:25:00 +0100

    merjd

--- python/merjd/merjd.py
+++ python/merjd/merjd.py
@@ -9,23 +9,28 @@ import numpy as np
 from scipy.io import wavfile
 
 
-file1 = sys.argv[1]
-file2 = sys.argv[2]
+files = sys.argv[1:]
 
-if file1.endswith('.mp3'):
-    fileout = file1.rsplit('.', 1)[0] + '.wav'
-    subprocess.call(['ffmpeg', file1, fileout])
-    file1 = fileout
+for i in files:
+    if i.endswith('.mp3'):
+        fileout = i.rsplit('.', 1)[0] + '.wav'
+        subprocess.call(['ffmpeg', '-i', i, fileout])       # needs -i to work this time?
+        i = fileout
 
-if file2.endswith('.mp3'):
-    fileout = file2.rsplit('.', 1)[0] + '.wav'
-    subprocess.call(['ffmpeg', file2, fileout])
-    file2 = fileout
-
-# all above: 2 x as much as i need :)
-
-sr1, audio1 = wavfile.read(file1)
-sr2, audio2 = wavfile.read(file2)
+sr1, audio1 = wavfile.read(files[0])
+sr2, audio2 = wavfile.read(files[1])
 
 # unlike skrejd, no need to make writeable copy of audio, as will create a 3rd file.
+
+# at the moment, test with 2 x glijd [same size]
+
+sys.exit()
+
+# done, write to newfile and finish
+
+nfile=input("Enter name for fileout: ")
+
+newfile = nfile.rsplit('.', 1)[0] + '.' + time.strftime("%d%H%M%S") + '.wav'
+print("Written to ", newfile)
+wavfile.write(newfile, sr, audio)
 
