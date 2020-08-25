8325054e264492d91b627265e1f1b8a3c90efd5a
jason <64638179+jawdu@users.noreply.github.com>
2020-08-25 19:11:26 +0100

    start lo-fi

--- python/skrejd/interface.py
+++ python/skrejd/interface.py
@@ -5,23 +5,24 @@ import sys
 # local modules
 import elements
 import fglitch
+import lofi
 import reverse
 import stretch
 import stutter
 
 def options(ns, audio):
     # maybe also write a log, of each option called. use subprocess. or do that in option, w/parameters
-    prompt = "\nSelect process, or '0' to write file and exit: \n 0: finish \n 1: reverse \n 2: stretch (slow) \n 3: slow glitch \n 4: stutter \n 5: flatten glitch \n ........................"
+    prompt = "\nSelect process, or '0' to write file and exit: \n 0: finish \n 1: reverse \n 2: stretch (slow) \n 3: slow glitch \n 4: stutter \n 5: flatten glitch \n 6: lo-fi \n ........................"
 
     while True:
         result = input(prompt)
         try:
             result = int(result) 
         except ValueError:
-            print("\nTry again, input type must be integer, 0-5 \n")
+            print("\nTry again, input type must be integer, 0-6 \n")
             continue
-        if not (0 <= result <= 5):
-            print("\nTry again, valid options are 0-5")
+        if not (0 <= result <= 6):
+            print("\nTry again, valid options are 0-6")
         elif (result == 0):
             print("\nFinished processing, going to output file and finish...\n")
             return
@@ -40,6 +41,9 @@ def options(ns, audio):
         elif (result == 5):
             print("\nGoing into flatten glitch...\n")        
             fglitch.fglitch(ns, audio)
+        elif (result == 6):
+            print("\nGoing into lo-fi...\n")        
+            lofi.lofi(ns, audio)
         else:
             # don't think it should ever get here? but anyway:
             return
---
+++ python/skrejd/lofi.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+# reduce bit/sample rate
+
+import inputs
+
+def lofi(ns, audio):
+    # over whole duration or usual chunks
+    # bitrate: modulo. samplerate: just double/triple bits? see if it works or not.
+
+    print("Finished lo-fi \n")
+    return
+
+
