28b76c854b01bb2fa5e72fe68045bd9d3b8323ea
jason <64638179+jawdu@users.noreply.github.com>
2020-10-16 22:54:14 +0100

    add input for filters

--- python/skrejd/filters.py
+++ python/skrejd/filters.py
@@ -21,11 +21,15 @@ def hpf(ns, audio):
     return
 
 def lpf(ns, audio):
-    print("\n into lpf...")
     # y[i] = b * x[i] + (1-b)y[i-1]  0 < b < 1   y is filtered; x unfiltered. b=1 = filter freq = sample rate (i.e. no change)
+    print("\n into lpf...")
+    
+    b = inputs.askfunc("Enter frequency (0-1), or 'd' for default: ", "frequency", float, 0.0, 1.0, 0.1)
+
+    # ask for degree of variablity? vary it gradually at random, also independent channel?
+
     audio[0, 0] = 0.0
     audio[0, 1] = 0.0
-    b = 0.1
     for i in range(1, ns):
         audio[i, 0] = b * audio[i, 0] + (1 - b) * audio[i-1, 0] 
         audio[i, 1] = b * audio[i, 1] + (1 - b) * audio[i-1, 1] 
