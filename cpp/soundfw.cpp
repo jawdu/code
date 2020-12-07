d395256472cc98d29fa98fdab2090752ab0c5b41
jason <64638179+jawdu@users.noreply.github.com>
2020-12-07 15:05:23 +0000

    more prep

--- cpp/soundfw.cpp
+++ cpp/soundfw.cpp
@@ -1,6 +1,10 @@
 // get writing .wav for cpp sorted
-// https://stackoverflow.com/a/31038528
 
+/* more to do
+
+https://stackoverflow.com/a/50916141 tog et filename with timestamp
+
+*/
 
 
 #include <iostream>
@@ -11,61 +15,70 @@ using namespace std;
 // global variables
 // function list
 
+void writeWav(string fileName);
 
 int main()
 {
-    // just test filename stuff 1st
 
+    // prepare date stamp next as well
     std::string fileName = "test.wav";
-
     cout << endl << "filename: " << fileName << endl;
-
+    
+    writeWav(fileName);
+ 
     return 0;
 
 }
 
 // functions
 
-
-/*
-
-// https://stackoverflow.com/a/50916141 tog et filename with timestamp
-
-
-ofstream stream;
-stream.open("sine.wav", ios::out | ios::binary);
-// iftsream myfile(file_name.c_str()); if not cpp11
-
-int bufferSize = (1/frequency)*samplerate; 
-stream.write("RIFF", 4);                    // RIFF chunk
-write<int>(stream, 36 + bufferSize*sizeof(int)); // RIFF chunk size in bytes
-stream.write("WAVE", 4);                    // WAVE chunk
-stream.write("fmt ", 4);                    // fmt chunk
-write32(stream, 16);                     // size of fmt chunk
-write16(stream, 1);                       // Format = PCM
-write16(stream, 1);                       // # of Channels
-write32(stream, samplerate);                // Sample Rate
-write32(stream, samplerate*sizeof(int));    // Byte rate
-write16(stream, sizeof(int));             // Frame size
-write16(stream, 24);                      // Bits per sample
-stream.write("data", 4);                   // data chunk
-write32(stream, bufferSize*sizeof(int));   // data chunk size in bytes
-
-
-// Now that the header is out of the way, you'll just need to modify your loop to first convert the double (-1.0,1.0) samples into 32-bit signed int. Truncate the bottom 8-bits since you only want 24-bit and then write out the data. Just so you know, it is common practice to store 24-bit samples inside of a 32-bit word because it is much easier to stride through using native types.
-
-for (int i = 0; i < bufferSize; ++i) // 
+void writeWav(string fileName)
 {
-    double tmp = 0.0;  // what the osund is (or grab from array)
-    int intVal = (int)(tmp * 2147483647.0) & 0xffffff00;
-    stream << intVal;
+    // https://stackoverflow.com/a/31038528
+
+    //https://www.reddit.com/r/cpp/comments/85uzkr/github_supersimple_intro_program_for_generating/
+    //^ compare and contrast?
+
+
+    ofstream stream;
+    stream.open(fileName.c_str(), ios::out | ios::binary);
+
+/*    double samplerate = 44100.00; // Sample rate
+    double frequency = 200.00; // Frequency
+
+    int bufferSize = (1/frequency)*samplerate; 
+    stream.write("RIFF", 4);                    // RIFF chunk
+    write<int>(stream, 36 + bufferSize*sizeof(int)); // RIFF chunk size in bytes
+    stream.write("WAVE", 4);                    // WAVE chunk
+    stream.write("fmt ", 4);                    // fmt chunk
+    write32(stream, 16);                     // size of fmt chunk
+    write16(stream, 1);                       // Format = PCM
+    write16(stream, 1);                       // # of Channels
+    write32(stream, samplerate);                // Sample Rate  
+    write32(stream, samplerate*sizeof(int));    // Byte rate
+    write16(stream, sizeof(int));             // Frame size
+    write16(stream, 24);                      // Bits per sample
+    stream.write("data", 4);                   // data chunk
+    write32(stream, bufferSize*sizeof(int));   // data chunk size in bytes
+
+
+    // Now that the header is out of the way, you'll just need to modify your loop to first convert the double (-1.0,1.0) samples into 32-bit signed int. Truncate the bottom 8-bits since you only want 24-bit and then write out the data. Just so you know, it is common practice to store 24-bit samples inside of a 32-bit word because it is much easier to stride through using native types.
+
+    for (int i = 0; i < bufferSize; ++i) // 
+    {
+        double tmp = 0.0;  // what the osund is (or grab from array) so this is dummy val for now
+        int intVal = (int)(tmp * 2147483647.0) & 0xffffff00;
+        stream << intVal;
+    }
+*/
+    return 0;
+
 }
 
 
-https://www.reddit.com/r/cpp/comments/85uzkr/github_supersimple_intro_program_for_generating/
-^ compare and contrast?
 
-*/
+
+
 
 
 
