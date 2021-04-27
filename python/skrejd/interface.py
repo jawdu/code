53e04a1c457a0e8f678d71fca4ca7f5a7ccfa0ed
jason <64638179+jawdu@users.noreply.github.com>
2021-04-27 17:05:50 +0100

    set up skrejd for reverb

--- python/skrejd/interface.py
+++ python/skrejd/interface.py
@@ -7,23 +7,24 @@ import elements
 import fglitch
 import filters
 import lofi
+import reverb
 import reverse
 import stretch
 import stutter
 
 def options(ns, audio):
     # maybe also write a log, of each option called. use subprocess. or do that in option, w/parameters
-    prompt = "\nSelect process, or '0' to write file and exit: \n 0: finish \n 1: reverse \n 2: stretch (slow) \n 3: slow glitch \n 4: stutter \n 5: flatten glitch \n 6: lo-fi \n 7: filter \n ........................"
+    prompt = "\nSelect process, or '0' to write file and exit: \n 0: finish \n 1: reverse \n 2: stretch (slow) \n 3: slow glitch \n 4: stutter \n 5: flatten glitch \n 6: lo-fi \n 7: filter \n 8: reverb \n ........................"
 
     while True:
         result = input(prompt)
         try:
             result = int(result) 
         except ValueError:
-            print("\nTry again, input type must be integer, 0-7 \n")
+            print("\nTry again, input type must be integer, 0-8 \n")
             continue
-        if not (0 <= result <= 7):
-            print("\nTry again, valid options are 0-7")
+        if not (0 <= result <= 8):
+            print("\nTry again, valid options are 0-8")
         elif (result == 0):
             print("\nFinished processing, going to output file and finish...\n")
             return
@@ -48,6 +49,9 @@ def options(ns, audio):
         elif (result == 7):
             print("\nGoing into filters...\n")  
             filters.filters(ns,audio)
+        elif (result == 8):
+            print("\nGoing into reverb...\n")  
+            reverb.reverb(ns,audio)
         # something where... I kinda map it onto a sine (or other periodic function?)
         else:
             # don't think it should ever get here? but anyway:
---
+++ python/skrejd/reverb.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+# reverbs???
+
+import inputs
+import random
+
+def reverb(ns, audio):
+
+    #print("Note, reverb may take over a minute for longer files, whole-file, and higher bitrates.... \n" )
+    
+    print("Finished reverb \n")
+    return
+
+
